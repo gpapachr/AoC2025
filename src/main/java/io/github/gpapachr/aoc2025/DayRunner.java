3df6e067ec5a217c85157a64af72fe8b44e1f07a
gpapachr <giannis.papachristou@yahoo.gr>
2026-01-09 11:23:49 +0200

    Refactor DayRunner to simplify execution per day

--- src/main/java/io/github/gpapachr/aoc2025/DayRunner.java
+++ src/main/java/io/github/gpapachr/aoc2025/DayRunner.java
@@ -1,7 +1,19 @@
 package io.github.gpapachr.aoc2025;
 
 import io.github.gpapachr.aoc2025.commons.InputReader;
+import lombok.NoArgsConstructor;
 
-public abstract class DayRunner {
-    protected static final InputReader inputReader = InputReader.getInstance();
+@NoArgsConstructor
+public final class DayRunner {
+    private static final InputReader inputReader = InputReader.getInstance();
+
+    public static void run(Day day) {
+        var input = inputReader.readMainInputLines(day.dayNumber());
+
+        var part1 = day.solvePart1(input);
+        var part2 = day.solvePart2(input);
+
+        System.out.println("Day " + day.dayNumber() + " - Part 1: " + part1);
+        System.out.println("Day " + day.dayNumber() + " - Part 2: " + part2);
+    }
 }
--- src/main/java/io/github/gpapachr/aoc2025/day01/Day01Runner.java
+++ src/main/java/io/github/gpapachr/aoc2025/day01/Day01Runner.java
@@ -1,19 +1,9 @@
 package io.github.gpapachr.aoc2025.day01;
 
-import io.github.gpapachr.aoc2025.Day;
 import io.github.gpapachr.aoc2025.DayRunner;
-import java.util.List;
 
-public class Day01Runner extends DayRunner {
+public class Day01Runner {
     public static void main(String[] args) {
-        Day day = new Day01();
-
-        List<String> input = inputReader.readMainInputLines(day.dayNumber());
-
-        int part1 = day.solvePart1(input).intValue();
-        int part2 = day.solvePart2(input).intValue();
-
-        System.out.println("Day " + day.dayNumber() + " - Part 1: " + part1);
-        System.out.println("Day " + day.dayNumber() + " - Part 2: " + part2);
+        DayRunner.run(new Day01());
     }
 }
